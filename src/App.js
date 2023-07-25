de4bbd62328503dc89185d54ff8f122cbf5e6038
Prathya Thakur <98422626+thakurprathya@users.noreply.github.com>
2023-07-25 21:20:16 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
 
-function App() {
+const Card = ({ imageUrl, description }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <img 
+          src={imageUrl} alt="img"
+          onError={(e) => {
+              e.target.onerror = null;
+              e.target.src = 'https://shorturl.at/aiqsA';
+          }}
+      />
+      <p>{description}</p>
     </div>
   );
+};
+
+
+const App = () =>{
+  const items = [
+    {
+      imageUrl: 'https://shorturl.at/EHOW5', //correct url
+      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley `,
+    },
+    {
+      imageUrl: 'ahttps://shorturl.at/EHOW5a',  //incorrect url
+      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley `,
+    },
+  ];
+
+    return(
+        <div className='app'>
+            {items.map((item, index)=>
+                <Card key={index} imageUrl={item?.imageUrl} description={item?.description} />
+            )}
+        </div>
+    );
 }
 
 export default App;
