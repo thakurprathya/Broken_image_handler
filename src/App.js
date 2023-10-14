9582d586160cdaf782bcf16c8bc234ea4ba49d6d
Prathya Thakur <prathya2410@gmail.com>
2023-10-14 22:33:58 +0530

    Update App.js, updated links to images

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const Card = ({ imageUrl, description }) => {
           src={imageUrl} alt="img"
           onError={(e) => {
               e.target.onerror = null;
-              e.target.src = 'https://shorturl.at/aiqsA';
+              e.target.src = 'https://media.geeksforgeeks.org/wp-content/uploads/20230802153215/Error-404.png';
           }}
       />
       <p>{description}</p>
@@ -19,11 +19,11 @@ const Card = ({ imageUrl, description }) => {
 const App = () =>{
   const items = [
     {
-      imageUrl: 'https://shorturl.at/EHOW5', //correct url
+      imageUrl: 'https://thenewcode.com/assets/images/thumbnails/homer-simpson.svg', //correct url
       description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley `,
     },
     {
-      imageUrl: 'ahttps://shorturl.at/EHOW5a',  //incorrect url
+      imageUrl: 'ahttps://thenewcode.com/assets/images/thumbnails/homer-simpson.svga',  //incorrect url
       description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley `,
     },
   ];
